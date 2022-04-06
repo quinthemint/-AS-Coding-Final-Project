4c0f9831b4f245b5d71cfef7bb232500df4067e1
quinthemint <59623248+quinthemint@users.noreply.github.com>
2022-04-06 10:47:42 -0700

    working gosling game

--- kaboom.js
+++ kaboom.js
@@ -2,8 +2,8 @@ import kaboom from "https://unpkg.com/kaboom/dist/kaboom.mjs"
 
 // initialize kaboom context
 export const k = kaboom({
-    width: 320,
-    height: 240,
+    width: 1200,
+    height: 600,
     font: "sinko",
     canvas: document.querySelector("#mycanvas"),
     background: [ 0, 0, 255, ],
--- main.js
+++ main.js
@@ -4,18 +4,43 @@
 import {k} from "./kaboom.js"
 
 // Load assets
-loadSprite("bean", "/sprites/bladerunner.jpeg")
+loadSprite("gosling", "/sprites/bladerunner.jpeg")
+loadSprite("enemy", "/sprites/download.jpeg")
 
 // Define player movement speed (pixels per second)
 const SPEED = 320
 
 // Add player game object
 const player = add([
-	sprite("bean"),
+	sprite("gosling"),
 	// center() returns the center point vec2(width() / 2, height() / 2)
-	pos(center()),
+  pos(center()),
+	area(),
+	// body() component gives the ability to respond to gravity
+	body(),
 ])
 
+add([
+	rect(width(), 48),
+	outline(4),
+	area(),
+	pos(0, height() - 48),
+	// Give objects a solid() component if you don't want other solid objects pass through
+	solid(),
+])
+
+add([
+		sprite("enemy"),
+		pos(40,40),
+		// Both objects must have area() component to enable collision detection between
+		area(),
+		"enemy",
+])
+
+player.onCollide("enemy", (enemy) => {
+	destroy(enemy)
+})
+  
 // onKeyDown() registers an event that runs every frame as long as user is holding a certain key
 onKeyDown("left", () => {
 	// .move() is provided by pos() component, move by pixels per second
@@ -27,12 +52,16 @@ onKeyDown("right", () => {
 })
 
 onKeyDown("up", () => {
-	player.move(0, -SPEED)
+if (player.isGrounded()) {
+		// .jump() is provided by body()
+		player.jump()
+	}
 })
 
 onKeyDown("down", () => {
 	player.move(0, SPEED)
 })
+
 
 // onClick() registers an event that runs once when left mouse is clicked
 onClick(() => {

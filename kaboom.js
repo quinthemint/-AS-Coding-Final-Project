0034b297f11abc4e6eecf3bc3bb9ee0e19c40405
changaimaraSA <35507028+changaimaraSA@users.noreply.github.com>
2022-04-15 16:21:15 -0700

    Working version of the leaf follow code.

--- kaboom.js
+++ kaboom.js
@@ -3,7 +3,7 @@ import kaboom from "https://unpkg.com/kaboom/dist/kaboom.mjs"
 // initialize kaboom context
 export const k = kaboom({
     width: 640,
-    height: 300,
+    height: 320,
     scale: 2,
     font: "sinko",
     canvas: document.querySelector("#mycanvas"),
